5638b97e7c2af6f92be6a53aa0b2d08f7b5a4cc4
Vieko Franetovic <vieko.franetovic@gmail.com>
2020-09-13 19:12:32 +0000

    testing codespaces

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ const Index = () => (
   <div>
     {/* CONTENT */}
     {/* SECTIONS */}
-    <Styled.h1>Hello CGDA</Styled.h1>
+    <Styled.h1>Hello Vieko</Styled.h1>
     {/* FOOTER */}
   </div>
 )
